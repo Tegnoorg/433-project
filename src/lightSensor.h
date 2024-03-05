ca0234174eda039497161feb4b14a1b3a7bc8123
Sunderinq <tegnoor.gill@hotmail.com>
2024-03-04 16:45:32 -0800

    added a function to get light sensor reading in voltage.

---
+++ src/lightSensor.h
@@ -0,0 +1,7 @@
+//header file containing the prototypes of functions used to read the light sensor values
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+//get voltage reading
+int getVoltage1Reading();
