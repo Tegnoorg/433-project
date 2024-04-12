dab796ed41a7027abab24e35125db234c1cb5b42
Teg <tegnoor.gill@gmail.com>
2024-04-12 00:40:06 -0700

    sad

---
+++ build/hal/CMakeFiles/hal.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hal.dir/src/forceSensor.c.o"
+  "CMakeFiles/hal.dir/src/forceSensor.c.o.d"
+  "CMakeFiles/hal.dir/src/hardwareCommands.c.o"
+  "CMakeFiles/hal.dir/src/hardwareCommands.c.o.d"
+  "CMakeFiles/hal.dir/src/icm20948.c.o"
+  "CMakeFiles/hal.dir/src/icm20948.c.o.d"
+  "CMakeFiles/hal.dir/src/joystick.c.o"
+  "CMakeFiles/hal.dir/src/joystick.c.o.d"
+  "CMakeFiles/hal.dir/src/lightSensor.c.o"
+  "CMakeFiles/hal.dir/src/lightSensor.c.o.d"
+  "CMakeFiles/hal.dir/src/motionSensor.c.o"
+  "CMakeFiles/hal.dir/src/motionSensor.c.o.d"
+  "libhal.a"
+  "libhal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
